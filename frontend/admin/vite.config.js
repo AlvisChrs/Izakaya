036fc0208d65b41d1f07311b28f565642331a7b2
AlvisChrs <alvischristian19@gmail.com>
2026-09-14 18:53:57 +0700

    fix: set vite base path to fix blank pages in production

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/admin/',
   plugins: [react()],
   build: {
     outDir: '../../public/admin',
